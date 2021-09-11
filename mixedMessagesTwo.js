c658442e260219201b0d51842ec1ef460bf7b163
jakedanielturner <jakedanielturner@gmail.com>
2021-09-11 23:06:40 +0100

    added to data object

--- mixedMessagesTwo.js
+++ mixedMessagesTwo.js
@@ -2,14 +2,14 @@
 // This file is intended to look more like the project file provided by Codecademy, including objects and interations
 
 const data = {
-    'weatherTypes': ['Sunny', 'Rainy', 'Thunderstormy', 'Windy', 'Snowy'],
+    'weatherTypes': ['Sunny', 'Rainy', 'Thunderstormy', 'Windy', 'Snowy', 'Foggy', 'Cloudy'],
     'daysOfWeek': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
     'cities': ['London', 'Birmingham', 'Leeds', 'Sheffield', 'Manchester', 'Newcastle', 'Belfast', 'Cardiff', 'Edinburgh', 'Glasgow']
 }
 
 const randomNumberGenerator = num => {
     generated = Math.floor(Math.random() * num);
-    
+
     return generated;
 }
 
