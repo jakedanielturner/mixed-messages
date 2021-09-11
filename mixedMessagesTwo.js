bdb73ae221937a6c71c1698f18045193532e8e1c
jakedanielturner <jakedanielturner@gmail.com>
2021-09-11 20:14:48 +0100

    added data object

--- mixedMessagesTwo.js
+++ mixedMessagesTwo.js
@@ -1,3 +1,9 @@
 // Mixed Messages second project file
 // This file is intended to look more like the project file provided by Codecademy, including objects and interations
 
+const data = {
+    'weatherTypes': [],
+    'daysOfWeek': [],
+    'cities': []
+}
+
