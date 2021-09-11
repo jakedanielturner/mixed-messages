93fd008e4e6e306d18473250e7e49706a5868a7b
jakedanielturner <jakedanielturner@gmail.com>
2021-09-11 18:37:54 +0100

    added output and console.log functionality

--- mixedMessages.js
+++ mixedMessages.js
@@ -6,3 +6,13 @@ var temperature = Math.floor(Math.random() * 40);
 
 var humidity = Math.floor(Math.random() * 100);
 
+function output () {
+    var selectedWeatherType = weatherTypes[(Math.floor(Math.random() * weatherTypes.length))];
+
+    weatherOutput = `The weather is ${selectedWeatherType}, with a temperature of ${temperature} and a humidity of ${humidity}%.`
+
+    return weatherOutput;
+}
+
+console.log(output());
+
