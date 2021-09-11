cc850f49ccb33bd725e14e8cf8d6750b6cb1f0b5
jakedanielturner <jakedanielturner@gmail.com>
2021-09-11 20:23:54 +0100

    added random number generator, added selection from data objection functionality

--- mixedMessagesTwo.js
+++ mixedMessagesTwo.js
@@ -2,8 +2,19 @@
 // This file is intended to look more like the project file provided by Codecademy, including objects and interations
 
 const data = {
-    'weatherTypes': [],
+    'weatherTypes': ['Sunny', 'Rainy', 'Thunderstormy', 'Windy', 'Snowy'],
     'daysOfWeek': [],
     'cities': []
 }
 
+const randomNumberGenerator = (num) => {
+    generated = Math.floor(Math.random() * num);
+    return generated;
+}
+
+const dataSelection = (obj) => {
+    const selectedWeatherType = obj.weatherTypes[randomNumberGenerator(obj.weatherTypes.length)];
+    return selectedWeatherType;
+}
+
+console.log(dataSelection(data));
